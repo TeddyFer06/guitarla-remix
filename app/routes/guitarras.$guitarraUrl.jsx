810c20a96ccbbdfab96ed649644c3c2c76cfc567
TeddyFer06 <72159786+TeddyFer06@users.noreply.github.com>
2023-11-15 00:06:47 -0500

    Advance with the project-Show the guitar with its information

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import { getGuitarra } from '~/models/guitarras.server'
+import { useLoaderData } from '@remix-run/react'
+import styles from '~/styles/guitarras.css'
+
+export function meta({ data }) {
+    return [
+        {
+            title: `GuitarLA - ${data.data[0].attributes.nombre}`
+        },
+        {
+            description: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.nombre}`
+        }
+    ]
+}
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }]
+}
 
 export async function loader({ params }) {
     const { guitarraUrl } = params
-    console.log(guitarraUrl)
-    return {}
+
+    const guitarra = await getGuitarra(guitarraUrl);
+
+    return guitarra;
 }
 
 const Guitarra = () => {
+
+    const guitarra = useLoaderData();
+    const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
+
     return (
-        <div>guitarras.$guitarraUrl</div>
+        <main className='contenedor guitarra'>
+            <img className='imagen' src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
+
+            <div className='contenido'>
+                <h3>{nombre}</h3>
+                <p className='texto'>{descripcion}</p>
+                <p className='precip'>${precio}</p>
+            </div>
+        </main>
     )
 }
 
